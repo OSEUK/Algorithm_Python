49ff058272292ed0b194020180d96a58986d6539
OSEUK <tpdnrzmzm3@naver.com>
2024-02-11 20:14:59 +0900

    소수 구하기

---
+++ 알고리즘 기초 1/수학 1/1929.py
@@ -0,0 +1,21 @@
+# 백준 1929, 소수 구하기
+
+if __name__ == "__main__":
+    M, N = map(int, input().split())
+
+    result = []
+    
+    for i in range(M, N+1):
+        is_prime = True
+        for j in range(2, int(i**(1/2))+1):
+            if (i%j) == 0:
+                is_prime = False
+                break
+            else:
+                continue
+        
+        if is_prime:
+            result.append(i)
+    
+    for r in result:
+        print(r)
