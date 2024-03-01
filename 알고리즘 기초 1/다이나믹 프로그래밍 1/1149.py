a51f485b439336b00d1d6f076563b8c17526a0e9
OSEUK <tpdnrzmzm3@naver.com>
2024-03-01 13:26:23 +0900

    RGB거리

---
+++ 알고리즘 기초 1/다이나믹 프로그래밍 1/1149.py
@@ -0,0 +1,25 @@
+# 백준 1149, RGB거리
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+arr = [[0]*3 for _ in range(N)]
+dp = [[0]*3 for _ in range(N)]
+
+for i in range(N):
+    arr[i] = list(map(int, input().split()))
+
+dp[0][0] = arr[0][0]
+dp[0][1] = arr[0][1]
+dp[0][2] = arr[0][2]
+
+for i in range(1, N):
+    dp[i][0] = arr[i][0] + min(dp[i-1][1], dp[i-1][2]) 
+    dp[i][1] = arr[i][1] + min(dp[i-1][0], dp[i-1][2])
+    dp[i][2] = arr[i][2] + min(dp[i-1][0], dp[i-1][1])
+
+print(min(dp[N-1]))
+
+
