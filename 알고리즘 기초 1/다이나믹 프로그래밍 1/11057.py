a2ece9803d1ef4ab3bfc035a986e58abb9ad0e14
OSEUK <tpdnrzmzm3@naver.com>
2024-03-03 11:03:31 +0900

    오르막 수

---
+++ 알고리즘 기초 1/다이나믹 프로그래밍 1/11057.py
@@ -0,0 +1,20 @@
+# 백준 11057, 오르막 수
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+dp = [[0]*10 for _ in range(N+1)]
+
+for i in range(10):
+    dp[1][i] = 1
+
+for i in range(2, N+1):
+    for j in range(10):
+        for k in range(j, -1, -1):
+            dp[i][j] += dp[i-1][k] % 10007
+
+print(sum(dp[N]) % 10007)
+
+
