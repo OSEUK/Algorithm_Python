6ffd38fb61fed706be2592fc8c82b15ce7f9500d
OSEUK <tpdnrzmzm3@naver.com>
2024-03-07 09:45:55 +0900

    가장 큰 증가하는 부분 수열

--- 알고리즘 기초 1/다이나믹 프로그래밍 1/11053.py
+++ 알고리즘 기초 1/다이나믹 프로그래밍 1/11053.py
@@ -1,13 +1,13 @@
 # 백준 11053, 가장 긴 증가하는 부분 수열
 
 n = int(input())
-arr = list(map(int, input().split()))
+sequence = list(map(int, input().split()))
 
-dp = [1] * n 
+dp = [1] * n
 
 for i in range(1, n):
     for j in range(i):
-        if arr[i] > arr[j]:
+        if sequence[i] > sequence[j]:
             dp[i] = max(dp[i], dp[j] + 1)
 
 print(max(dp))
