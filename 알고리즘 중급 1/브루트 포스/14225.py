ff3a157ca539328ede939cc4a3adc86286acfc58
OSEUK <tpdnrzmzm3@naver.com>
2024-05-11 17:13:55 +0900

    부분수열의 합

--- 알고리즘 중급 1/브루트 포스/14225.py
+++ 알고리즘 중급 1/브루트 포스/14225.py
@@ -6,19 +6,16 @@ S = list(map(int, input().split()))
 visited = [False] * N
 used_sum = [False] * 10000000
 
-def dfs(S, used_sum, sum):
+def dfs(index, current_sum):
+    if index == N:
+        if current_sum > 0:
+            used_sum[current_sum] = True
+        return
+    
+    dfs(index + 1, current_sum + S[index])
+    dfs(index + 1, current_sum)
 
-    used_sum[sum] = True
-
-    for i in range(N):
-        if not visited[i]:
-            next_sum = sum + S[i]
-            if next_sum < len(used_sum) and not used_sum[sum + S[i]]:
-                visited[i] = True
-                dfs(S, used_sum, sum + S[i])        
-                visited[i] = False
-
-dfs(S, 0)
+dfs(0,0)
 
 for i in range(1, 10000000):
     if not used_sum[i]:
