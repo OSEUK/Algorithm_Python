c179b3130882108059a8b4b7f8ef95c34c5266a5
OSEUK <tpdnrzmzm3@naver.com>
2024-05-17 13:29:50 +0900

    N-Queen

--- 알고리즘 중급 1/브루트 포스/9663.py
+++ 알고리즘 중급 1/브루트 포스/9663.py
@@ -1,18 +1,25 @@
 # 백준 9663, N-Queen (GOLD IV)
 
 N = int(input())
-board = [[0] * N for _ in range(N)]
-visited = [[False] * N for _ in range(N)]
+board = [0] * N
+count = 0
 
-count_queens = 0
+def is_safe(row, col):
+    for i in range(row):
+        if board[i] == col or board[i] - i == col - row or board[i] + i == col + row:
+            return False
+    return True
 
-def dfs(board, row):
-    global count_queens
-
-    if row == N-1:
-        count_queens += 1
-        return 
+def backtrack(row):
+    global count
+    if row == N:
+        count += 1
+    for col in range(N):
+        if is_safe(row, col):
+            board[row] = col
+            backtrack(row + 1)
+            board[row] = 0
     
-    for i in range(N):
-        if not visited[row][i]:
-            visited[row] = [True] * N
+backtrack(0)
+
+print(count)
